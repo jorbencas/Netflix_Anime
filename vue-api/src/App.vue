31c3a3843865665995c5a625233b24b71c40d8f1
jorbencas <jorbencas@gmail.com>
2020-12-25 13:01:18 +0100

    refactoring dependences

--- vue-api/src/App.vue
+++ vue-api/src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
