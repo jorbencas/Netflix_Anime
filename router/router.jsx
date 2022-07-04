4272fbc32b70d5c21fe099fa4882d3d255f04416
jorbencas <jorbencas@gmail.com>
2022-07-04 21:18:24 +0200

    Revert

--- router/router.jsx
+++ router/router.jsx
@@ -15,6 +15,119 @@ import Counter from "../components/counter/counter.jsx";
 const RouterApp = () => {
   return (
     // <Router hook={useHashLocation}>
+<<<<<<< HEAD:router/router.jsx
+=======
+    <Switch>
+      <Route path="/info">
+        <Redirect to="/about" />
+      </Route>
+      <Route path="/">This example uses hash-based routing.</Route>
+      <Route path="/about">
+        <article>
+          <h1>Wouter API</h1>
+
+          <p>
+            A tiny routing solution for modern React apps that relies on Hooks.
+            A router you wanted so bad in your project!
+          </p>
+          <p>
+            Wouter comes with two kinds of APIs: low-level{" "}
+            <a href="https://reactjs.org/docs/hooks-intro.html" rel="nofollow">
+              React Hooks
+            </a>{" "}
+            API and more traditional component-based API similar to React
+            Router's one.
+          </p>
+          <p>
+            <ul>
+              <li>
+                Zero dependency, only <strong>1151 B</strong> gzipped vs 17KB{" "}
+                <a href="https://github.com/ReactTraining/react-router">
+                  React Router
+                </a>
+                .
+              </li>
+              <li>
+                Supports both <strong>React</strong> and{" "}
+                <strong>
+                  <a href="https://preactjs.com/" rel="nofollow">
+                    Preact
+                  </a>
+                </strong>
+                ! Read{" "}
+                <em>
+                  <a href="#preact-support">"Preact support" section</a>
+                </em>{" "}
+                for more details.
+              </li>
+              <li>
+                No top-level <code>&lt;Router /&gt;</code> component, it is{" "}
+                <strong>fully optional</strong>.
+              </li>
+              <li>
+                Mimics{" "}
+                <a href="https://github.com/ReactTraining/react-router">
+                  React Router
+                </a>
+                's best practices by providing familiar
+                <strong>
+                  <a href="#route-pathpattern-">
+                    <code>Route</code>
+                  </a>
+                </strong>
+                ,{" "}
+                <strong>
+                  <a href="#link-hrefpath-">
+                    <code>Link</code>
+                  </a>
+                </strong>
+                ,{" "}
+                <strong>
+                  <a href="#switch-">
+                    <code>Switch</code>
+                  </a>
+                </strong>{" "}
+                and{" "}
+                <strong>
+                  <a href="#redirect-topath-">
+                    <code>Redirect</code>
+                  </a>
+                </strong>{" "}
+                components.
+              </li>
+              <li>
+                Has hook-based API for more granular control over routing (like
+                animations):{" "}
+                <strong>
+                  <a href="#uselocation-hook-working-with-the-history">
+                    <code>useLocation</code>
+                  </a>
+                </strong>
+                ,{" "}
+                <strong>
+                  <a href="#useroute-the-power-of-hooks">
+                    <code>useRoute</code>
+                  </a>
+                </strong>{" "}
+                and{" "}
+                <strong>
+                  <a href="#userouter-accessing-the-router-object">
+                    <code>useRouter</code>
+                  </a>
+                </strong>
+                .
+              </li>
+            </ul>
+          </p>
+        </article>
+      </Route>
+      <Route path="/:anything*">
+        <center>
+          <b>404:</b> Sorry, this page isn't ready yet!
+        </center>
+      </Route>
+
+>>>>>>> parent of d08a803... Revert "Test Router":src/router/router.jsx
       {/* <Route path="/">
           <>
             <h1>Hola Mundo</h1>
@@ -24,7 +137,11 @@ const RouterApp = () => {
           <Counter />
         </Route>
         <Route path="/edit" component={Edit} /> */}
+<<<<<<< HEAD:router/router.jsx
       /*<Route path="/about" component={About} />
+=======
+      {/*<Route path="/about" component={About} />
+>>>>>>> parent of d08a803... Revert "Test Router":src/router/router.jsx
           <Route exact path="/" component={Home} />
           <Route
             path={["/Anime", "/filters/:filter", "/Anime/od", "/Anime/oa"]}
@@ -52,7 +169,11 @@ const RouterApp = () => {
           <Route path="/Collection/:id" component={Collection} />
           <Route path='/History' component={History} />
           <Route path='/EditDetail/:id' component={Edit}/> */}
+<<<<<<< HEAD:router/router.jsx
 
+=======
+    </Switch>
+>>>>>>> parent of d08a803... Revert "Test Router":src/router/router.jsx
     // </Router>
   );
 };
